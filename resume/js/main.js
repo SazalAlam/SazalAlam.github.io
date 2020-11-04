3c45c1c4e11912f9040c398c539f75100e4c8479
Sazal Alam <sazal.alam@gmail.com>
2020-11-04 22:04:26 +0600

    My Resume

---
+++ resume/js/main.js
@@ -0,0 +1,48 @@
+$(document).ready(function () {
+
+
+
+
+
+
+    //owl carousel
+
+    $('.site-main .about-area .owl-carousel').owlCarousel({
+        loop: true,
+        autoplay: true,
+        dots: true,
+        responsive: {
+            0: {
+                items: 1
+            },
+            544: {
+                items: 2
+            }
+        }
+    })
+
+    //sticky nav menu
+
+    let nav_offset_top = $('.header_area').height() + 50;
+
+    function navbarFixed() {
+        if ($('.header_area').length) {
+            $(window).scroll(function () {
+                let scroll = $(window).scrollTop();
+                if (scroll >= nav_offset_top) {
+                    $('.header_area .main-menu').addClass('navbar_fixed');
+                } else {
+                    $('.header_area .main-menu').removeClass('navbar_fixed');
+                }
+            })
+        }
+    }
+
+    navbarFixed();
+
+
+});
+
+
+
+
